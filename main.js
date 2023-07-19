d6ecf1957dc0f28d6a9912e193900ccc343b59ad
aleq <aleqpower@gmail.com>
2023-07-19 12:05:24 -0300

    Exercicio_Jquery

--- main.js
+++ main.js
@@ -1,29 +1,14 @@
-$(document).ready(function () {
-  // document.getElementById("botao-adicionar")
-  const botaoAdicionar = $("#botao-adicionar");
-  // garantir que eu peguei o elemento certo
-  console.log("botaoAdicionar", botaoAdicionar);
-
-  botaoAdicionar.click(function () {
-    // fazer alguma coisa
-    console.log("Hello");
-
-    // 1 pegar o text input
-    const textInput = $("#nome");
-
-    // 2 pegar o texto dentro desse text input
-    const textInputValue = textInput.val();
-    console.log("Valor do input de texto", textInputValue);
-
-    // 3 pegar o ul elemento do HTML e trazer para jsLand
-    const listaCompleta = $("#Lista-completa");
-    console.log($("#Lista-completa"))
-
-    // 4 criar o li com o valor do input
-    // 5 pegar o texto e colocar dentro da lista
-    $('<li />', {
-        text: textInputValue
-    }).appendTo(listaCompleta);
-
+$(function(){
+  $("#botao-adicionar").click(function(){
+    var nome=$("#nome").prop("value");
+    var atual=$("#lista-completa").html();
+    $("#lista-completa").html(atual+"<li>"+nome+"</li>");
   });
+  function taxado(){
+    $("li").click(function(){
+      $(this).addClass("decorar");
+    })
+  }
+  setInterval(taxado,100);
 });
+
